13adfd129e892315f467287d4f0c5dbff84721bc
Rachel Lin <rachel.lin91@gmail.com>
2015-06-10 04:46:06 +0800

    added topics controlled and crud actions

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,16 +1,48 @@
 class TopicsController < ApplicationController
   def index
+    @topics = Topic.all
   end
 
   def new
+    @topic = Topic.new
   end
 
+     def create
+       @topic = Topic.new(params.require(:topic).permit(:name, :description, :public))
+       authorize @topic
+       if @topic.save
+         redirect_to @topic, notice: "Topic was saved successfully."
+       else
+         flash[:error] = "Error creating topic. Please try again."
+         render :new
+       end
+     end
+
   def show
+    @topic = Topic.find(params[:id])
   end
 
   def edit
+    @topic = Topic.find(params[:id])
   end
 
+    def update
+       @topic = Topic.find(params[:id])
+       authorize @topic
+       if @topic.update_attributes(params.require(:topic).permit(:name, :description, :public))
+         redirect_to @topic
+       else
+         flash[:error] = "Error saving topic. Please try again."
+         render :edit
+       end
+     end
+
   def destroy
+    @topic = Topic.find(params[:id])
+    @topic.destroy
+    redirect_to @topics
   end
+
+
+ 
 end
